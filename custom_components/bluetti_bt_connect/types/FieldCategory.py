5c92f0c756edfb1408bff8c6117a4688a1c09e4c
Elliottmonaghan <138760999+Elliottmonaghan@users.noreply.github.com>
2026-07-27 17:28:28 +1000

    Fresh start

---
+++ custom_components/bluetti_bt_connect/types/FieldCategory.py
@@ -0,0 +1,37 @@
+from homeassistant.const import EntityCategory
+from bluetti_bt_connect_lib import FieldName
+
+
+DIAGNOSTICS = [
+    FieldName.DEVICE_SN,
+    FieldName.DEVICE_TYPE,
+    FieldName.VER_ARM,
+    FieldName.VER_DSP,
+    FieldName.VER_BMS,
+    FieldName.PACK_CELL_VOLTAGES,
+]
+
+CONFIGS = [
+    FieldName.CTRL_CHARGING_MODE,
+    FieldName.CTRL_DISPLAY_TIMEOUT,
+    FieldName.CTRL_ECO,
+    FieldName.CTRL_ECO_AC,
+    FieldName.CTRL_ECO_DC,
+    FieldName.CTRL_ECO_MIN_POWER_AC,
+    FieldName.CTRL_ECO_MIN_POWER_DC,
+    FieldName.CTRL_ECO_TIME_MODE,
+    FieldName.CTRL_ECO_TIME_MODE_AC,
+    FieldName.CTRL_ECO_TIME_MODE_DC,
+    FieldName.CTRL_POWER_LIFTING,
+    FieldName.CTRL_SPLIT_PHASE,
+    FieldName.CTRL_UPS_MODE,
+    FieldName.GRID_EXPORT_ENABLED,
+]
+
+
+def get_category(field: FieldName) -> EntityCategory | None:
+    if field in DIAGNOSTICS:
+        return EntityCategory.DIAGNOSTIC
+    if field in CONFIGS:
+        return EntityCategory.CONFIG
+    return None
