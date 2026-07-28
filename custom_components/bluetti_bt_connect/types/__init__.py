f771b69575ef9c8cfc819452a8c4cce63697cd29
Elliottmonaghan <138760999+Elliottmonaghan@users.noreply.github.com>
2026-07-28 20:18:15 +1000

    revert to fresh start

--- custom_components/bluetti_bt_connect/types/__init__.py
+++
@@ -1,8 +0,0 @@
-from .FullDeviceConfig import *
-from .InitialDeviceConfig import *
-from .ManufacturerData import *
-from .OptionalDeviceConfig import *
-
-from .FieldCategory import *
-from .FieldDeviceClass import *
-from .FieldStateClass import *
