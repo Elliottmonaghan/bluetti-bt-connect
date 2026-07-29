8afd56d8802dcd96f5b0dc88651b46bc01f24443
Elliottmonaghan <138760999+Elliottmonaghan@users.noreply.github.com>
2026-07-29 17:06:36 +1000

    revert to fresh start

--- custom_components/bluetti_bt_connect/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for the Bluetti BT Connect integration."""
-
-DOMAIN = "bluetti_bt_connect"
-MANUFACTURER = "Bluetti"
-
-CONF_OPTIONS = "options"
-
-DATA_COORDINATOR = "coordinator"
-DATA_LOCK = "lock"
